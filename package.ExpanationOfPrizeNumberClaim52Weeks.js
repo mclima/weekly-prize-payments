a7909f68b90554ca313de33d6aef2b6c7e8041fe
mclima <mcamil2009@gmail.com>
2023-12-25 09:33:05 -0500

    Delete console.logs

--- package.ExpanationOfPrizeNumberClaim52Weeks.js
+++ package.ExpanationOfPrizeNumberClaim52Weeks.js
@@ -1,7 +1,6 @@
 //2022.10.05 - Calculate dates and prizes - MCL
 
 const awardDate = '4/30/22';
-// http://creativedept.classic.pchad.com/Libraries/Packages/Spectrum/phase4/house/Expanation_Of_Prize_Number_Claim/5k_AWFL_V2/page1.html?score=Group1
 
 //variables to update as needed
 const numberDaysInWeek = 7;
@@ -20,16 +19,11 @@ CalculateDatesPrizes = {
 
             let date = new Date(awardDate.replace(/-/g,"/"));
             let epncPayment = x * epncWeeklyPayment;
-            
-            // console.log(x)
 
              //the winner receives the first prize on award date
             date.setDate(date.getDate() + (x-1)*7);
-
             
-            // console.log(date);
-            let epncDate = (date.getMonth()+1) + '/' + date.getDate() + '/' + date.getFullYear().toString().slice(-2); 
-            // console.log(epncDate);    
+            let epncDate = (date.getMonth()+1) + '/' + date.getDate() + '/' + date.getFullYear().toString().slice(-2);    
             
             html += `<li class="eopnc-record ${ (i===1) ?  ' grey-bg' :''} ${ (i===2) ?  ' epnc-padding' :''} ${ (i===3) ?  ' blue-highlight' :''}"> <span class="eopnc-date">+ ${epncDate} </span><span class="eopnc-description">Weekly Deposit</span> <span class="eopnc-amount">$5,000.00</span> <span class="eopnc-avail-balance">${CalculateDatesPrizes.helper.formatToCurrency(epncPayment)}</span> </li>`;
 
@@ -39,18 +33,10 @@ CalculateDatesPrizes = {
 
             let epncYear = new Date(awardDate.replace(/-/g,"/"));
 
-            // let getYear = (epncYear.getFullYear() + y);
             let getYearRounded = (epncYear.getFullYear() + y).toString().slice(-2);
             let getMonth = epncYear.getMonth() + 1;
             let getDay = epncYear.getDate();
-
-            console.log(`get new Date ${epncYear}`)
-            // console.log(`year number ${x}`)
-            // console.log(`year = ${getYear}`);
-            console.log(`month = ${getMonth}`);
-            // yearFinal = `${getMonth}/${getDay}/${getYear}`;
             let yearFinalPrint = `${getMonth}/${getDay}/${getYearRounded}`;
-            // balanceFinal = (CalculateDatesPrizes.helper.calculateYearsBetweenDates(awardDate,yearFinal) * numberWeeksInYear * epncWeeklyPayment);
             let balanceFinal = epncYears[i] * numberWeeksInYear * epncWeeklyPayment;
 
             html += `<li class="eopnc-record eopnc-stmt">Statement as of ${yearFinalPrint} (Year ${y})</li>`;
@@ -66,16 +52,7 @@ CalculateDatesPrizes = {
         epncYears.forEach((x, i) => {
 
             const epncYear = new Date(awardDate.replace(/-/g,"/"));
-
-            // epncYear.setDate(epncYear.getFullYear() + x);
-
             let getFullYear = epncYear.getFullYear() + x;
-
-            // let getNumberOfWeeks = CalculateDatesPrizes.getISOWeeks(epncYear.getFullYear() + x);
-            console.log(`get new Date ${epncYear}`)
-            console.log(`year number ${x}`)
-            console.log(`full year = ${getFullYear}`)
-            // console.log(`get number of weeks ${getNumberOfWeeks}`);
 
         });
 
@@ -87,11 +64,6 @@ CalculateDatesPrizes = {
       
         d = new Date(y, 0, 1);
         isLeap = new Date(y, 1, 29).getMonth() === 1;
-
-        // console.log(`d = ${d}`);
-        // console.log(`isLeap = ${isLeap}`)
-        // console.log(`d.getDay() = ${d.getDay()}`)
-        // console.log(`isLeap && d.getDay() ${isLeap && d.getDay()}`)
       
         //check for a Jan 1 that's a Thursday or a leap year that has a 
         //Wednesday jan 1. Otherwise it's 52
@@ -135,25 +107,14 @@ CalculateDatesPrizes = {
                 currency: 'USD',
             });
             
-            return formatter.format(number); /* $2,500.00 */
+            return formatter.format(number); 
 
         }
            
-  
     }
-      
-
 
 };
 
 CalculateDatesPrizes.renderHTML(awardDate, epncWeeks);
 CalculateDatesPrizes.getFullYear(awardDate, epncYears);
-
-// console.log(CalculateDatesPrizes.getISOWeeks(2019))
-// console.log(CalculateDatesPrizes.getISOWeeks(2020))
-// console.log(CalculateDatesPrizes.getISOWeeks(2021))
-// console.log(CalculateDatesPrizes.getISOWeeks(2022))
-// console.log(CalculateDatesPrizes.getISOWeeks(2023))
-console.log( CalculateDatesPrizes.helper.calculateWeeksBetweenDates(awardDate,'4/30/2062') )
-console.log( CalculateDatesPrizes.helper.calculateWeeksBetweenDates(awardDate,'4/30/2062') *5000)
 
